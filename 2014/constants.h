46d5f71123536d87f49b2e1ed2062e194a703a34
shrimpydubsy <new.joseph10@gmail.com>
2014-01-29 18:54:08 -0500

    Update constants.h

--- 2014/constants.h
+++ 2014/constants.h
@@ -6,4 +6,6 @@ static const UINT32 RIGHT_MOTOR_PORT = 2;
 static const UINT32 JOYSTICK_PORT = 1;
 static const UINT32 ARM_MOTOR_PORT = 3;
 
+static const float ARM_SPEED = 0;
+
 #endif /* CONSTANTS */
