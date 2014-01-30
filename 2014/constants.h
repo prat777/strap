df6b33f45b9021ec4f71087dfc6af62ef1888d22
shrimpydubsy <new.joseph10@gmail.com>
2014-01-30 16:56:25 -0500

    Update constants.h

--- 2014/constants.h
+++ 2014/constants.h
@@ -1,11 +1,18 @@
 #ifndef CONSTANTS
 #define CONSTANTS
 
+// Drive system motor ports
 static const UINT32 LEFT_MOTOR_PORT = 1;
 static const UINT32 RIGHT_MOTOR_PORT = 2;
+
+// Driver joystick ports
 static const UINT32 JOYSTICK_PORT = 1;
+
+// Arm motor ports
 static const UINT32 ARM_MOTOR_PORT = 3;
 
-static const float ARM_SPEED = 0;
+// Pneumatic system ports
+static const UINT32 SPIKE_RELAY_PORT = 2;
+static const UINT32 PRESSURE_SWITCH_PORT = 4;
 
 #endif /* CONSTANTS */
