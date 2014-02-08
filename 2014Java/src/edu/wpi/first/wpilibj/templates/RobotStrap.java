3a0389f80d618dcf26fa70a8520fe00916ac822b
shrimpboyho <shrimpboyhoftw@gmail.com>
2014-02-08 13:43:05 -0500

    Got the drive system finalized, and the conveyor motor system finalized, and the compressor finalized.
    Signed-off-by: shrimpboyho <shrimpboyhoftw@gmail.com>

--- 2014Java/src/edu/wpi/first/wpilibj/templates/RobotStrap.java
+++ 2014Java/src/edu/wpi/first/wpilibj/templates/RobotStrap.java
@@ -11,34 +11,48 @@ import edu.wpi.first.wpilibj.DriverStationLCD;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStationLCD.Line;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Victor;
 
 
 public class RobotStrap extends SimpleRobot
 {
     /* Set up all the objects that we will be using */
-    private RobotDrive drivetrain;
-    private Joystick leftStick;
-    private Joystick rightStick;
-    private Jaguar arm;
-    private Compressor c;
-    private DigitalInput fwdlim;
-    private DigitalInput fwdlim;
+    private final RobotDrive drivetrain;
+    private final Joystick leftStick;
+    private final Joystick rightStick;
+    private final Jaguar arm1;
+    private final Jaguar arm2;
+    private final Jaguar conv1;
+    private final Victor conv2;
+    private final Compressor c;
+    private final DigitalInput fwdlim;
+    private final DigitalInput bwdlim;
+    private final Solenoid s1;
     
     /* Set up all the constant values */
     // Drive system motor ports
-    private final int LEFT_MOTOR_PORT = 1;
-    private final int RIGHT_MOTOR_PORT = 2;
+    private final int FRONT_LEFT_MOTOR_PORT = 1;
+    private final int FRONT_RIGHT_MOTOR_PORT = 2;
+    private final int REAR_LEFT_MOTOR_PORT = 3;
+    private final int REAR_RIGHT_MOTOR_PORT = 4;
     
     // Driver joystick ports
     private final int LEFT_JOYSTICK_PORT = 1;
     private final int RIGHT_JOYSTICK_PORT = 2;
     
     // Arm motor ports
-    private final int ARM_MOTOR_PORT = 3;
+    private final int ARM1_MOTOR_PORT = 7;
+    private final int ARM2_MOTOR_PORT = 8;
+    
+    // Conveyor motor ports
+    private final int CONV1_MOTOR_PORT = 5;
+    private final int CONV2_MOTOR_PORT = 6;
     
     // Pneumatic system ports
     private final int SPIKE_RELAY_PORT = 1;
-    private final int PRESSURE_SWITCH_PORT = 8;
+    private final int PRESSURE_SWITCH_PORT = 1;
     
     // Limit switch ports
     private final int FWD_LIM_PORT = 10;
@@ -50,13 +64,17 @@ public class RobotStrap extends SimpleRobot
         // Throw caution to the wind
         getWatchdog().setEnabled(false);
         
-        drivetrain = new RobotDrive(LEFT_MOTOR_PORT, RIGHT_MOTOR_PORT);
+        drivetrain = new RobotDrive(FRONT_LEFT_MOTOR_PORT, REAR_LEFT_MOTOR_PORT, FRONT_RIGHT_MOTOR_PORT, REAR_RIGHT_MOTOR_PORT);
         leftStick = new Joystick(LEFT_JOYSTICK_PORT);
         rightStick = new Joystick(RIGHT_JOYSTICK_PORT);
-        arm = new Jaguar(ARM_MOTOR_PORT);
+        arm1 = new Jaguar(ARM1_MOTOR_PORT);
+        arm2 = new Jaguar(ARM2_MOTOR_PORT);
     	c = new Compressor(PRESSURE_SWITCH_PORT, SPIKE_RELAY_PORT);
-    	fwdlim = new DigitalInput(FWD_LIM_PORT);
+    	conv1 = new Jaguar(CONV1_MOTOR_PORT);
+        conv2 = new Victor(CONV2_MOTOR_PORT);
+        fwdlim = new DigitalInput(FWD_LIM_PORT);
     	bwdlim = new DigitalInput(BWD_LIM_PORT);
+        s1 = new Solenoid(1);
     }
     
     /* Autonomous */
@@ -74,26 +92,36 @@ public class RobotStrap extends SimpleRobot
         
         while (true && isOperatorControl() && isEnabled())
         {
-            
             // Print status if the button is pressed
-        	if(rightStick.getRawButton(11))
-    			printStatus();
+            if(rightStick.getRawButton(11))
+    		printStatus();
+            
+            // get conveyor stuff going
+            boolean upValue = leftStick.getRawButton(3);
+            boolean downValue = leftStick.getRawButton(2);
+            
+            double speedConv = (leftStick.getAxis(Joystick.AxisType.kZ) + (-1)) / 2 * -1;
+            
+            if(upValue)
+            {
+                conv1.set(2 * speedConv);
+                conv2.set(1 * speedConv);
+            }
+            else if(downValue)
+            {
+                conv1.set(-2 * speedConv);
+                conv2.set(-1 * speedConv);
+            }
+            else
+            {
+                conv1.set(0);
+                conv2.set(0);
+            } 
+            
+    	    // Determine the speed to throw the arm at
+    	    //double speed = (rightStick.getAxis(Joystick.AxisType.kY) - (-1)) / 2;
     
-    		// Move the arm based on buttons
-    		boolean launchValue = rightStick.getRawButton(1);
-    		boolean rewindValue = rightStick.getRawButton(2);
-    
-    		// Determine the speed to throw the arm at
-    		double speed = (rightStick.getThrottle() - (-1)) / 2;
-    
-    		if(launchValue /* && fwdlim.get() */)
-    			// make arm launch forwards
-    			arm.set(1 * speed);
-    		else if(rewindValue /* && bwdlim.get() */)
-    			// make the arm launch backwards
-    			arm.set(-1 * speed);
-    		else
-    			arm.set(0);
+    	    //arm.set(rightStick.getAxis(Joystick.AxisType.kY));
             
             // arcade drive
             drivetrain.arcadeDrive(rightStick);
@@ -117,6 +145,14 @@ public class RobotStrap extends SimpleRobot
     
         // Print the voltage
         dslcd.println(DriverStationLCD.Line.kUser6, 1, "Battery Voltage: " + String.valueOf(voltage));
+        dslcd.updateLCD();
+    }
+
+    /* Print a message to the user message center */
+    public void printStuff(String msg, Line type)
+    {
+        DriverStationLCD dslcd = DriverStationLCD.getInstance();
+        dslcd.println(type, 1, msg);
         dslcd.updateLCD();
     }
     
