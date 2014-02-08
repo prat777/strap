680a83f6fd4d4bbd53d1ff8767c2ae91f0e04d64
shrimpboyho <shrimpboyhoftw@gmail.com>
2014-02-08 16:16:57 -0500

    Got the digital input working.
    Signed-off-by: shrimpboyho <shrimpboyhoftw@gmail.com>

--- 2014Java/src/edu/wpi/first/wpilibj/templates/RobotStrap.java
+++ 2014Java/src/edu/wpi/first/wpilibj/templates/RobotStrap.java
@@ -27,9 +27,11 @@ public class RobotStrap extends SimpleRobot
     private final Jaguar conv1;
     private final Victor conv2;
     private final Compressor c;
-    private final DigitalInput fwdlim;
-    private final DigitalInput bwdlim;
-    private final Solenoid s1;
+    private final DigitalInput armfwdlim;
+    private final DigitalInput armbwdlim;
+    private final Solenoid latch;
+    private final Solenoid s2;
+    private final Solenoid s3;
     
     /* Set up all the constant values */
     // Drive system motor ports
@@ -55,8 +57,8 @@ public class RobotStrap extends SimpleRobot
     private final int PRESSURE_SWITCH_PORT = 1;
     
     // Limit switch ports
-    private final int FWD_LIM_PORT = 10;
-    private final int BWD_LIM_PORT = 9;
+    private final int ARM_FWD_LIM_PORT = 3;
+    private final int ARM_BWD_LIM_PORT = 2;
     
     /* Constructor */
     public RobotStrap()
@@ -72,9 +74,11 @@ public class RobotStrap extends SimpleRobot
     	c = new Compressor(PRESSURE_SWITCH_PORT, SPIKE_RELAY_PORT);
     	conv1 = new Jaguar(CONV1_MOTOR_PORT);
         conv2 = new Victor(CONV2_MOTOR_PORT);
-        fwdlim = new DigitalInput(FWD_LIM_PORT);
-    	bwdlim = new DigitalInput(BWD_LIM_PORT);
-        s1 = new Solenoid(1);
+        armfwdlim = new DigitalInput(ARM_FWD_LIM_PORT);
+    	armbwdlim = new DigitalInput(ARM_BWD_LIM_PORT);
+        latch = new Solenoid(1);
+        s2 = new Solenoid(2);
+        s3 = new Solenoid(3);
     }
     
     /* Autonomous */
@@ -87,8 +91,11 @@ public class RobotStrap extends SimpleRobot
     /* Telo-op */
     public void operatorControl()
     {
-    	// Start the auto compressor manager
-    	c.start();
+    	// Start the auto compressor manager and hold all solenoids
+    	latch.set(latch.get());
+        s2.set(s2.get());
+        s3.set(s3.get());
+        c.start();
         
         while (true && isOperatorControl() && isEnabled())
         {
@@ -96,12 +103,18 @@ public class RobotStrap extends SimpleRobot
             if(rightStick.getRawButton(11))
     		printStatus();
             
+            // enable soleniod swapping
+            if(leftStick.getRawButton(7))
+                latch.set(!latch.get());
+            if(leftStick.getRawButton(6))
+                s2.set(!s2.get());
+            if(leftStick.getRawButton(8))
+                s3.set(!s3.get());
+            
             // get conveyor stuff going
             boolean upValue = leftStick.getRawButton(3);
             boolean downValue = leftStick.getRawButton(2);
-            
             double speedConv = (leftStick.getAxis(Joystick.AxisType.kZ) + (-1)) / 2 * -1;
-            
             if(upValue)
             {
                 conv1.set(2 * speedConv);
@@ -118,10 +131,24 @@ public class RobotStrap extends SimpleRobot
                 conv2.set(0);
             } 
             
+            // limit switch auto latching
+            if(armbwdlim.get())
+            {
+                latch.set(true);
+                printStuff("Latch fired by limit switch", Line.kUser3);
+            }
+            
     	    // Determine the speed to throw the arm at
-    	    //double speed = (rightStick.getAxis(Joystick.AxisType.kY) - (-1)) / 2;
-    
-    	    //arm.set(rightStick.getAxis(Joystick.AxisType.kY));
+            if(leftStick.getAxis(Joystick.AxisType.kY) >= 0 && !armfwdlim.get())
+            {
+                arm1.set(leftStick.getAxis(Joystick.AxisType.kY));
+                arm2.set(leftStick.getAxis(Joystick.AxisType.kY));
+            }
+            if(leftStick.getAxis(Joystick.AxisType.kY) <= 0 && !armbwdlim.get())
+            {
+                arm1.set(leftStick.getAxis(Joystick.AxisType.kY));
+                arm2.set(leftStick.getAxis(Joystick.AxisType.kY));
+            }
             
             // arcade drive
             drivetrain.arcadeDrive(rightStick);
